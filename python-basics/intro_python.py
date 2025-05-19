44b59244dfbcf479bb1d9a0cf676e42bdd87060c
Buhle <mtshalizanobuhle7@gmail.com>
2025-05-19 20:22:03 +0200

    updated the python file

--- python-basics/intro_python.py
+++ python-basics/intro_python.py
@@ -1,4 +1,18 @@
 #variables in python
+
 #1. Strings & f'string formatter
 first_name = "Buhle Mtshali";
-print(f"my name is {first_name}");
+
+#2. Intergers in python (whole numbers)
+age = 23;
+
+#3. Floats in python(decimal numbers)
+price = 19.44;
+
+#4. Boolean Expressions(TRUE/FALSE)
+is_student = False;
+
+if is_student:
+    print(f"My name is {first_name}, my age is {age} my school jersey costs R{price} and I'm a student");
+else:
+    print("I am not a student!")
