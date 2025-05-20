db952e7cff1f44b7e926343e867d1ff55913cc60
Buhle <mtshalizanobuhle7@gmail.com>
2025-05-20 20:05:12 +0200

    updated the age calculator

---
+++ mini-age-calculator/age_calculator.py
@@ -0,0 +1,5 @@
+user_name = int(input("Enter your name: "))
+birth_year = int(input("Enter your Birth Year: "))
+current_year = int(input("Enter the Year: "));
+age = current_year - birth_year;
+print(age)
