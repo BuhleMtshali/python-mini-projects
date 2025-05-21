95f219763a29315f5f2f19dd58276e4217a7d401
Buhle <mtshalizanobuhle7@gmail.com>
2025-05-21 19:12:47 +0200

    updated the game logic

--- madlibs-game/madlibs.py
+++ madlibs-game/madlibs.py
@@ -1,3 +1,11 @@
 #1. adjective
 #2. noun
-#Yesterday i was {adjective} and i saw a {noun} and my first thought was to {adjective2} and also {adjective3} which was when i saw {noun2}ß
+#Yesterday i was {adjective} and i saw a {noun} and my first thought was to {adjective2} and also {adjective3} which was when i saw a {noun2}
+adjective = input("Enter an adjective: ");
+noun = input("Enter a noun: ");
+adjective2 = input("Enter another adjective: ")
+adjective3 = input("Enter another adjective: ")
+noun2 = input("Enter another noun: ")
+print("------Madlibs Game Output👾-----")
+print(f"Yesterday i was {adjective} and i saw a {noun} and my first thought was to {adjective2} and also {adjective3} which was when i saw a {noun2}");
+print("-----Game Over⏳----")
