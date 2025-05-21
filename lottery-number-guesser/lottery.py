5239e95b4aac608e514ac02428eb38d29b9db3ca
Buhle <mtshalizanobuhle7@gmail.com>
2025-05-21 19:31:01 +0200

    updated the lottery file

--- lottery-number-guesser/lottery.py
+++ lottery-number-guesser/lottery.py
@@ -6,3 +6,8 @@ number4 = int(input("Enter your fourth Lottery Number🤑: "));
 number5 = int(input("Enter your fifth Lottery Number🤑: "));
 number6 = int(input("Enter your final Lottery Number💵: "));
 powerball = int(input("Enter your lucky Powerball Number🏦: "));
+
+print(f"----Welcome {user_name} to Your Lottery Guessing Game💸----");
+print(f"Your lottery numbers are as follows: {number1}, {number2}, {number3}, {number4}, {number5}, {number6}");
+print(f"Your Lucky Powerball Number: {powerball}");
+print(f"---Goodluck {user_name} on your Lottery Numbers📝!")
