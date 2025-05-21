929b10d3286310f5611f6547b251e0fb42cf2fca
Buhle <mtshalizanobuhle7@gmail.com>
2025-05-21 19:17:31 +0200

    updated the madlibs game

--- madlibs-game/madlibs.py
+++ madlibs-game/madlibs.py
@@ -1,6 +1,4 @@
-#1. adjective
-#2. noun
-#Yesterday i was {adjective} and i saw a {noun} and my first thought was to {adjective2} and also {adjective3} which was when i saw a {noun2}
+
 adjective = input("Enter an adjective: ");
 noun = input("Enter a noun: ");
 adjective2 = input("Enter another adjective: ")
