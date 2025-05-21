538b1b1d1f05e98166eee5ca9014944e19e39c61
Buhle <mtshalizanobuhle7@gmail.com>
2025-05-21 19:51:14 +0200

    updated the tip calculator

--- tips-calculator/tips_calculator.py
+++ tips-calculator/tips_calculator.py
@@ -4,3 +4,5 @@ order_total = float(input("Enter your Order Total🍱: "));
 people = int(input("How many people are you splitting the amount with👭: "));
 tip_percentage = float(input("What percentage is your tip💰: "));
 tip_amount = tip_percentage / 100;
+total_amount = order_total + tip_amount;
+amount_per_person = total_amount / people;
