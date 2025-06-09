13d81e3f7b7a39e0cfef46f18c8c49ad39e40701
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-09 22:07:47 +0200

    updated the variables folder

--- variable-methods/Variables.py
+++ variable-methods/Variables.py
@@ -4,4 +4,11 @@ full_name = f" {first_name} {last_name}";
 print(f"My name is {full_name.title()}");
 
 #adding tabs in python
-print("\tHello Python")
+print("\tHello Python");
+
+#printing a string in a new line
+print("\nPython\nJava\nC++");
+
+#removing a url
+example_link = "https://example.com";
+print(example_link.removeprefix('https://'));
