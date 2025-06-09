41f526808a1c018980ce3a3ce3be3eecab0529b6
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-09 22:40:34 +0200

    updated the number file

---
+++ variable-methods/numbers.py
@@ -0,0 +1,19 @@
+#addition
+addition = 3 + 5;
+print(f"Addition: {addition}");
+
+#minus
+minus = 9 - 5;
+print(f"Minus: {minus}");
+
+#division
+divide = 6 / 3;
+print(f"Divide: {divide}");
+
+#multiplication
+multiply = 4 * 5;
+print(f"Multiplication: {multiply}");
+
+#exponents
+exponents = 2 ** 4;
+print(f"Exponents: {exponents}")
