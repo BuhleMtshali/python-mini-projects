70f7d312b77f5b404f390b487d9048aae49d6c03
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-09 21:54:42 +0200

    updated the variables folder

--- variable-methods/Variables.py
+++ variable-methods/Variables.py
@@ -1,4 +1,7 @@
 first_name = "buhle";
 last_name = "mtshali";
-full_name = f"My name is {first_name} {last_name}";
-print(full_name.title());
+full_name = f" {first_name} {last_name}";
+print(f"My name is {full_name.title()}");
+
+#adding tabs in python
+print("\tHello Python")
