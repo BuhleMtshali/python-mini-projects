c266d712a4793ee691959d2032870dbef3cf7ac7
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-13 11:17:15 +0200

    updated the lists file

--- lists-python/lists.py
+++ lists-python/lists.py
@@ -1,3 +1,7 @@
 #lists in python
 example_lists = ["Mary", "John", "Dave", "Seth"];
-print(example_lists)
+print(example_lists);
+
+#accessing elements within  a list
+index_one = example_lists[0];
+print(index_one); #prints the element at index one
