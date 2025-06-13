c138e4a37fd6a9480e06bf1dd57abd3a59507a65
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-13 16:01:27 +0200

    updated the guest file

--- lists-python/guest_list.py
+++ lists-python/guest_list.py
@@ -1,5 +1,13 @@
 guests = ["kate", "linda", "robert", "kim", "tonya", "zack", "ed", "mike"] #this is the list of peopl i will be inviting\
 
+#addign more people to the list
+guests.insert(0, "meg");
+guests.insert(5, "todd");
+guests.append("zendaya")
+
 #I will use a loop to send each of them an invite
 for guest in guests:
-    print(f"Good day {guest.title()}, I would like to formally invite you to my current event taking place tomorrow!");
+    if guest == "ed":
+        print(f"Thank for your RSVP {guest}, its a shame you wont be thre!");
+    else:
+        print(f"Good day {guest.title()}, I would like to formally invite you to my current event taking place tomorrow!");
