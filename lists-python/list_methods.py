58714929e26e9d693c228ffca743d9ff128f79c6
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-13 16:46:15 +0200

    updated the list methods

---
+++ lists-python/list_methods.py
@@ -0,0 +1,9 @@
+list_example = ["ray", "josh", "mikaela", "janine", "rose", "mandy"];
+
+#modifying an element by its position
+list_example[0] = "rachel";
+print(f"Modified list: {list_example}");
+
+#adding an element to array
+added_list = list_example.append("zakery");
+print(f"Appended list: {list_example}");
