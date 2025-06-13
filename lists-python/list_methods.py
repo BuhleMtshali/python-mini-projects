4fb4ed1881da7d08961fc43205cc5eef932ebe88
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-13 17:37:10 +0200

    updated methods to modify a list

--- lists-python/list_methods.py
+++ lists-python/list_methods.py
@@ -15,3 +15,15 @@ print(f"Sorted list: {list_example}");
 #sorting a list in descending order
 list_example.sort(reverse=True);
 print(f"Descending Order: {list_example}");
+
+#sorting based on custom rule 
+list_example.sort(key=len);  #sorts it based on the length of the words
+print(f"Sorting with custom rule: {list_example}");
+
+#returning a new sorted list without mordifying the original
+copy_sorted_list = sorted(list_example); #returns a list in alphabetical order
+print(f"New Sorted list: {copy_sorted_list}");
+
+#reverse sorted list
+copy_reverse_list = sorted(list_example, reverse=True);
+print(f"Reversed  Copy: {copy_reverse_list}");
