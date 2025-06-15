d52204537afc13a42026a0280285c782a5ec8f22
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-15 16:01:52 +0200

    updated slicing example

--- lists-python/slicing_list.py
+++ lists-python/slicing_list.py
@@ -6,3 +6,6 @@ print(f"Length of the List: {len(slicing_example)}")
 
 #slicing the list
 print(f"Sliced list: {slicing_example[2:6]}") #returns items from index 2 to 5
+
+#slicing by omiting the end value
+print(f"Slicing from second value onwards: {slicing_example[3:]}") #prints from mikaela to the end
