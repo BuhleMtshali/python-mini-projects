08f292023f3f7d4f453b8d6d1faf7f470d5f634e
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-15 15:54:23 +0200

    updated the slicing list

--- lists-python/slicing_list.py
+++ lists-python/slicing_list.py
@@ -1,1 +1,8 @@
 #example of slicing
+slicing_example = ["john", "dave", "john", "mikaela", "stella", "lizzy"];
+
+#finding the list first
+print(f"Length of the List: {len(slicing_example)}")
+
+#slicing the list
+print(f"Sliced list: {slicing_example[2:6]}") #returns items from index 2 to 5
