63c1697803190cda90c8afbaea0e5728ce1eb24d
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-15 06:38:50 +0200

    updated the loops file

--- loops-python/loops_example.py
+++ loops-python/loops_example.py
@@ -3,3 +3,4 @@ loop_list = ["mary", "john", "dave", "mike" ];
 #using the for loop
 for list in loop_list:
     print(f"Here is a looped list of names: {list.title()}");
+    print(f"Hello, {list}\n")
