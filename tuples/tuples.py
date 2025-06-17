4f25c7915b474aeed9d22e2f58abd60543545ee2
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-17 21:45:01 +0200

    updated the tuples list

--- tuples/tuples.py
+++ tuples/tuples.py
@@ -1,4 +1,9 @@
 #tuples
 tuple_example = (1, 3, 5, 7, 9);
 
-print(f"Length: {len(tuple_example)}")
+print(f"Length: {len(tuple_example)}");
+
+#trying to modify it
+#tuple_example[0] = 2;
+#print(tuple_example)
+
