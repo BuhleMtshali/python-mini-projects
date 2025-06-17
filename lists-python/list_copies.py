b2bd940a8952dc606010ef762bd191e7eaf77955
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-17 20:29:29 +0200

    updated the list of copies

--- lists-python/list_copies.py
+++ lists-python/list_copies.py
@@ -1,5 +1,6 @@
 original_lists = ["pizza", "burger", "chips", "wors"];
 copy_list = original_lists[:];
-
+original_lists.append("donuts");
+copy_list.append("ice cream")
 print(f"Original list: {original_lists}")
-print(f"Copy: {copy_list}")
+print(f"Copy: {copy_list}");
