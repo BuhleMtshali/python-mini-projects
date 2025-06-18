725dfebcfa52b3f34ab326e368263f6bcc28b421
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-18 21:33:20 +0200

    updated the array to sort from lowest to highest

--- if-statements-and-for-loops/if_statements.py
+++ if-statements-and-for-loops/if_statements.py
@@ -1,7 +1,8 @@
 ages = [23, 42, 36, 56, 17, 18, 29];
+ages.sort()
 
 for age in ages:
     if age >= 18:
         print(f"You're currently, {age} years old, therefore you're most welcome to the club!");
     else:
-        print("You are currently {age} years old, which is below the 18 years old minimum age required to enter the club!");
+        print(f"You are currently {age} years old, which is below the 18 years old minimum age required to enter the club!");
