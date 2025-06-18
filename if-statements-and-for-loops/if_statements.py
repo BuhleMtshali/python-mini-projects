144c53d3771c35a05cce52de6771f718eb86bf03
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-18 21:29:39 +0200

    updated the if statement

--- if-statements-and-for-loops/if_statements.py
+++ if-statements-and-for-loops/if_statements.py
@@ -1,1 +1,7 @@
-age = [23, 42, 36, 56, 17, 18, 29]
+ages = [23, 42, 36, 56, 17, 18, 29];
+
+for age in ages:
+    if age >= 18:
+        print(f"You're currently, {age} years old, therefore you're most welcome to the club!");
+    else:
+        print("You are currently {age} years old, which is below the 18 years old minimum age required to enter the club!");
