092659c5035ab4ee64268ae06fef562dcb9fb7f5
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-21 08:03:11 +0200

    updated the list

--- lists-python/editing_lists.py
+++ lists-python/editing_lists.py
@@ -1,2 +1,9 @@
 #to check if user exist we use "in"
+exists_list = ["john", "mike", "dave"];
+
+name_exists = "dave" in exists_list;
+
+print(f"Check if user exists: {name_exists}")
+
+
 #to check if they dont exist we use "not"
