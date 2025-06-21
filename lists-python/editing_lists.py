69729d45e41e3d63f73d8f538182c340efa3def2
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-21 08:25:45 +0200

    updated the editing list

--- lists-python/editing_lists.py
+++ lists-python/editing_lists.py
@@ -9,9 +9,10 @@ print(f"Check if user exists: {name_exists}")
 #to check if they dont exist in the current we use "not";
 banned_user = ["andrew", "levi", "sivvy"];
 user = "levi";
+
 if user not in banned_user:
-    banned_user.append(user)
-    print(f"{user.title()} you cannot post"); 
+    banned_user.append(user);
+    print(f"{user.title()} you have been added to users who cannot post");
+    print(f"Banned users: {banned_user}")
 else:
-    print(f"{user.title()} you're welcome to post");
-print(f"Users who can post: {banned_user}");
+    print(f"you cannot post {user}")
