56185c58e908bc7168809d0b272755b6646c5361
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-21 12:59:05 +0200

    updated the basic shopping cart

---
+++ mini-shopping-cart/basic_shopping_cart.py
@@ -0,0 +1,13 @@
+cart = [];
+
+while True:
+    print("\n==== Welcome to the Shopping Cart🛒 ==== ");
+    print("1. View Cart Items📋: ");
+    print("2. Add Item🥓: ");
+    print("3. Remove Item🗑️: ");
+    print("4. Exit Cart ❌:");
+
+    choice = input("\nPlease choose an option below📝: ");
+
+    if choice == "1":
+        
