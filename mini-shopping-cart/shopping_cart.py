d43e941d80b7049e063f2a1308a9a69279e21815
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-21 12:02:12 +0200

    updated the shopping cart

--- mini-shopping-cart/shopping_cart.py
+++
@@ -1,5 +0,0 @@
-item = input("What Item do you want to order? ");
-price = float(input("How much does it cost? "));
-quantity = int(input("How many would you like? "));
-total = price * quantity;
-print(f"You have ordered {quantity} x {item} and your order total is R{total}")
