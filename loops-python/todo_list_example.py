3746d037c18e8d8c27c3e56f1eb72f3c00f20a95
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-23 23:33:57 +0200

    updated the mini to do

---
+++ loops-python/todo_list_example.py
@@ -0,0 +1,14 @@
+to_do_list = [];
+
+print("======= Welcome to Mini Shopping Cart🛒 =======");
+name = input("Enter you name: ")
+print(f"==== Welcome to your shopping Cart Console {name}🙋🏻‍♀️ =====");
+
+#starting a while loop
+while True:
+    print("\n ---- 👾Please choose an option Below ---- ");
+    print("1. Add Item🍇: ");
+    print("2. Remove Item🗑️: ");
+    print("3. View Cart");
+
+    choice = input("Choose an option (1 - 4): ");
