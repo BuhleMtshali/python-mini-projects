e53fcc6ab114bb1989e1c27c705702a620c6e897
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-23 19:57:30 +0200

    updated the multiple condition statement

--- if-statements-and-for-loops/multiple_conditions.py
+++ if-statements-and-for-loops/multiple_conditions.py
@@ -1,9 +1,9 @@
 multiple_list = ['dave', 'ray', 'axon', 'phoebe', 'randal'];
 
-name = 'dave';
+name = 'Ray';
 
 #checking if only 1 example exists
-if name in multiple_list:
+if name.lower() in multiple_list:
     print(f"Hey {name}");
 else:
     print(f"hey stranger")
