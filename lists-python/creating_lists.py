5c4dd35b83451217aaf8deb35110ae199002d975
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-24 21:07:21 +0200

    updated the list file

---
+++ lists-python/creating_lists.py
@@ -0,0 +1,8 @@
+dog_lists = ['masey', 'ava', 'maccarooni', 'rex'];
+
+print("===== Welcome To The Dog Park🐶 =====")
+
+dog_name = input("Enter the name of your dog: ");
+
+for dog in dog_lists:
+    print(dog)
