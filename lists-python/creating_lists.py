77ed046c41856c2debe1336dd365b0020e3d5a48
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-24 21:15:51 +0200

    updated the list file

--- lists-python/creating_lists.py
+++ lists-python/creating_lists.py
@@ -6,11 +6,9 @@ dog_name = input("Enter the name of your dog: ");
 
 for dog in dog_lists:
     if dog_name in dog_lists:
-        print(f"{dog} is at the park")
         print(f"{dog_name} is already at the park");
     else:
         dog_lists.append(dog_name);
-        print(f"{dog_name} has arrived at the park!!!")
-        print(f"Here are the dogs at the park: {dog}")
+        print(f"{dog_name} has arrived at the park!!!");
 
         
