c7a93cce316682ce8e4725b5d0ea63864de109fa
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-25 23:29:40 +0200

    created the example file to practice dictionaries

---
+++ dictionaries/example.py
@@ -0,0 +1,20 @@
+users = [
+    {
+        "name": "buhle",
+        "age": 23,
+        "city": "durban",
+        "role": "admin"
+     },
+     {
+         "name": "luke",
+         "age": 16,
+         "city": "cape town",
+         "role": "user"
+     },
+     {
+         "name": "zakes",
+         "age": 65,
+         "city": "joburg",
+         "role": "user"
+     }
+    ]
