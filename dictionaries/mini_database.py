0a682ff54e4e56d1d3e0e102cfbb41776713b51a
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-26 00:04:01 +0200

    updated the mini database

--- dictionaries/mini_database.py
+++ dictionaries/mini_database.py
@@ -1,2 +1,24 @@
 #first define the list to store the users we will add
 users = [];
+
+print("\n====== Welcome to the Mini Database 👾 ======")
+
+#start the loop 
+while True:
+    print("\n==== Add New User🙋🏻‍♀️ =====");
+
+    #ask the user for these inputs
+    name = input("Enter name: ");
+    age = int(input("Enter age: "));
+    city = input("Enter City: ");
+    role = input("Enter role (admin/user): ");
+
+    #creating the dictionary
+    user = {
+        "name": name,
+        "age": age,
+        "city": city,
+        "role": role
+    }
+
+    #adding the user to the list
