bb92166dde52d7dad0e807cac4938203d15a3ad0
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-25 22:18:58 +0200

    updated the dictionaries

---
+++ dictionaries/dictionaries.py
@@ -0,0 +1,17 @@
+people = [
+    {
+        "name": "buhle",
+        "age": 23,
+        "city": "durban"
+     },
+     {
+         "name": "luke",
+         "age": 16,
+         "city": "cape town"
+     },
+     {
+         "name": "zakes",
+         "age": 65,
+         "city": "joburg"
+     }
+    ]
