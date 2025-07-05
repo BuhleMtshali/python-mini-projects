084693086893f711db15335fbd2d8869a945083e
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-05 15:33:09 +0200

    updated the inventory list

--- dictionaries/inventory_tracker.py
+++ dictionaries/inventory_tracker.py
@@ -2,4 +2,15 @@
 inventory = []
 
 #welcome message
-print()
+print("---- Welcome to the Mini Inventory Tracker🛒 ----");
+name = input("Enter your name: ");
+user_email = input("Enter your email: ")
+
+#start the while loop
+while True:
+    print("1. Add Item to inventory👾: ")
+    print("2. Viw Items in Inventory📝: ")
+    print("3. Remove Item🍀: ")
+    print("4. Exit❌: ")
+
+    option = input("\nPlease choose an option to continue: ")
