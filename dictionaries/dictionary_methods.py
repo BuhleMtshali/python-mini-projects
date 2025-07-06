4647039c1a293eff26a425d5422e7fc09d8bd9a5
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-06 17:00:24 +0200

    updated the dictionary app

--- dictionaries/dictionary_methods.py
+++ dictionaries/dictionary_methods.py
@@ -1,5 +1,5 @@
 example_dictionary = {'name': 'buhle', 'age': 23, 'profession': 'junior software developer'}
 
 #using the get() method
-get_value = example_dictionary.get('name', 'key value does not exist')
+get_value = example_dictionary.get('file', 'key value does not exist')
 print(get_value)
