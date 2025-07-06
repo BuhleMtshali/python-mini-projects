d1f4a65fd68a8631a00ac76abb066de97fbe7101
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-06 16:48:56 +0200

    updated the dictionary method

--- dictionaries/dictionary_methods.py
+++ dictionaries/dictionary_methods.py
@@ -1,1 +1,5 @@
 example_dictionary = {'name': 'buhle', 'age': 23, 'profession': 'junior software developer'}
+
+#using the get() method
+get_value = example_dictionary.get('name', 'key value does not exist')
+print(get_value)
