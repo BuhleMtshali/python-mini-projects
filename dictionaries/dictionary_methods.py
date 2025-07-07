99c549fe361b38b7dee7f1f27f1ad66365a39366
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-07 18:20:11 +0200

    updated the loop to loop through a list

--- dictionaries/dictionary_methods.py
+++ dictionaries/dictionary_methods.py
@@ -2,3 +2,7 @@ example_dictionary = [{'name': 'buhle', 'age': 23, 'profession': 'junior softwar
                       {'name': 'zac', 'age': 30, 'profession': 'senior software developer'},
                       {'name': 'linda', 'age': 27, 'profession': 'ui/ux designer'}]
 
+#looping through the list of dictionary
+for key, value in example_dictionary.items():
+    print(f"\nKey: {key}")
+    print(f"\nValue: {value}")
