6068e4423050b11065c469747fb0bb342c7b3a39
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-08 18:40:00 +0200

    updated the contact book

---
+++ dictionaries/contact_book.py
@@ -0,0 +1,12 @@
+#first define the list that will hold the added numbers
+contact_book = [];
+
+#print the welcome message
+welcome_message = "===== 👋🏻Welcome to you own Personalized Contact Book📲 ====="
+print(welcome_message)
+
+#get the user's details
+user_name = input("Enter your name👥: ");
+user_lastname = input("Enter your last nameℹ️: ");
+
+#validate the email
