531825af8be9f049e3ae2fd311037174cc14f336
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-10 18:14:23 +0200

    updated the nested dictionary app

---
+++ dictionaries/nested_dictionary.py
@@ -0,0 +1,12 @@
+pizza = {
+    'jane': ['chicken', 'cheese'],
+    'john': ['vegetarian', 'extra mushrooms', 'extra onion'],
+    'kate': 'no toppings',
+    'mike': ['chicken', 'mushroom', 'burger pizza', 'beef0']
+}
+
+#first loop in the dictionary
+for name, toppings in pizza.items():
+    print(f"{name.title()}'s favorite pizza toppings are: ")
+    for topping in toppings:
+        print(f"{topping.title()}")
