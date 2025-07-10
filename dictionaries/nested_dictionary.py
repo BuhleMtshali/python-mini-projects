915069a3f7cd9b92700507863c81cc47250de73d
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-10 18:20:55 +0200

    updated the print out message

--- dictionaries/nested_dictionary.py
+++ dictionaries/nested_dictionary.py
@@ -1,12 +1,16 @@
 pizza = {
     'jane': ['chicken', 'cheese'],
     'john': ['vegetarian', 'extra mushrooms', 'extra onion'],
-    'kate': ['no toppings'],
+    'kate': [],
     'mike': ['chicken', 'mushroom', 'burger pizza', 'beef']
 }
 
 #first loop in the dictionary
 for name, toppings in pizza.items():
     print(f"{name.title()}'s favorite pizza toppings are: ")
-    for topping in toppings:
-        print(f" - {topping.title()}")
+    if len(toppings) <= 0:
+        print(f"{name}, does not like any toppings on their pizza!!!")
+    else:
+
+        for topping in toppings:
+            print(f" - {topping.title()}")
