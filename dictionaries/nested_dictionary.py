3ca2aa4f40759e30b140fd333efde16fd8756322
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-10 18:37:50 +0200

    updated the nested dictionary

--- dictionaries/nested_dictionary.py
+++ dictionaries/nested_dictionary.py
@@ -1,3 +1,5 @@
+# Nesting a list in a dictionary
+
 pizza = {
     'jane': ['chicken', 'cheese'],
     'john': ['vegetarian', 'extra mushrooms', 'extra onion'],
@@ -14,3 +16,26 @@ for name, toppings in pizza.items():
 
         for topping in toppings:
             print(f" - {topping.title()}")
+
+#nesting a dictionary in a dictionary
+users = {
+    'joyce': {
+        'first': 'joycen',
+        'last': 'cage',
+        'location': 'durban',
+    },
+
+    'ryan': {
+        'first': 'rysalin',
+        'last': 'gosling',
+        'location': 'dundee'
+    }
+}
+
+for username, user_info in users.items():
+    print(f"\nUsername: {username}")
+    full_name = f"{user_info['first']} {user_info['last']}"
+    location = user_info['location']
+
+    print(f"\tFull Name: {full_name.title()}")
+    print(f"\tLocation: {location.title()}")
