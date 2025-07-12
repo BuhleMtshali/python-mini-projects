cfddf4f2c88fa765340ad7461e98fad376416085
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-12 15:35:11 +0200

    updated the fizzbuz

--- while-loops/fizzbuzz_while_loop.py
+++ while-loops/fizzbuzz_while_loop.py
@@ -1,9 +1,13 @@
 #example of while loop
-number = 0
+number = 1
 
-while number <= 10:
-    if number % 2 == 0:
-        print("FizzBuzz!!")
+while number <= 20:
+    if number % 3 == 0 and number % 5 == 0:
+        print('Double fizzbuzz')
+    elif number % 3 == 0:
+        print('fizz')
+    elif number % 5 == 0:
+        print('buzz')
     else:
         print(number)
     number += 1
