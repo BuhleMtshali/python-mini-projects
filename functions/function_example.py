83e2847d8199428a415d6a72fbcd47fee0f269f6
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-25 14:52:24 +0200

    updated the function

--- functions/function_example.py
+++ functions/function_example.py
@@ -1,6 +1,5 @@
 def greet_user(name):
     """Displaying a simple greeting"""
-    name = input("Enter your name: ")
     print(f"hello {name.title()}, this is my first function.")
 
 greet_user('buhle')
