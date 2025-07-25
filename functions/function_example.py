a4ce2c63a0d5537e76244807d85e4985ec9037dd
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-25 18:50:17 +0200

    added default parameters

--- functions/function_example.py
+++ functions/function_example.py
@@ -1,5 +1,6 @@
-def greet_user(name):
+def greet_user(name='stranger'):
     """Displaying a simple greeting"""
     print(f"hello {name.title()}, this is my first function.")
 
 greet_user('buhle')
+greet_user()
