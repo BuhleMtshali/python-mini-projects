c9625febb0b2b8313c893f8da8e89b6ea8e85241
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-25 19:19:30 +0200

    added multile arguments

--- functions/function_example.py
+++ functions/function_example.py
@@ -4,3 +4,10 @@ def greet_user(name='stranger'):
 
 greet_user('buhle')
 greet_user()
+
+#favorite book function
+def fav_book(name, book_name, book_author):
+    """Displaying user favorite book"""
+    print(f"Hey, my name is {name.title()} and my favorite book is {book_name.title()} by {book_author.title()}")
+
+fav_book('buhle','the mummy', 'brad pitt')
